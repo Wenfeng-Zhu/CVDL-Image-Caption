e50a7f9e2bbd50d41e2014f47c7cc5af34cb950c
Wenfeng Zhu <zhuwenfengerik@outlook.com>
2022-08-03 14:35:47 +0200

    finish the test part and test-output

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -4,33 +4,33 @@ import argparse
 
 def parse_arguments() -> Namespace:
     parser = argparse.ArgumentParser(
-        description="LT2326 H21 Mohamed's Project")
+        description="LMU-SS2022-CVDL-Project")
 
     parser.add_argument("--dataset_dir",
                         type=str,
-                        default="/srv/data/guszarzmo/mlproject/data/mscoco_h5",
-                        help="Directory contains processed MS COCO dataset_functional.")
+                        default="datasets-processed",
+                        help="Directory contains processed MS COCO dataset.")
 
     parser.add_argument("--save_dir",
                         type=str,
-                        default="/srv/data/guszarzmo/mlproject/data/test_out",
+                        default="test_output",
                         help="Directory to save the output files.")
 
     parser.add_argument("--config_path",
                         type=str,
-                        default="code/config.json",
+                        default="config.json",
                         help="Path for the configuration json file.")
 
     parser.add_argument("--checkpoint_name",
                         type=str,
-                        default="0102.1513/checkpoint_best.pth.tar",
-                        help="Path for the configuration json file.")
+                        default="0108.0132/checkpoint_best.pth.tar",
+                        help="Path for the checkpoint file.")
 
     parser.add_argument(
         '--device',
         type=str,
-        default="gpu",  # gpu, cpu, mgpu
-        help='path to pre-trained word Embedding.')
+        default="gpu",  # gpu, cpu
+        help='parameter of device')
 
     args = parser.parse_args()
 
